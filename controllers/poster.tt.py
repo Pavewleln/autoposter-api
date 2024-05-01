ff0e38bd1ac0724cf5984c46c20d9ea0202b0bee
pavel <kulikovps2004@gmail.com>
2024-05-01 23:50:27 +0300

    added README.md, writted poster.tt.py

--- controllers/poster.tt.py
+++ controllers/poster.tt.py
@@ -1,19 +1,26 @@
 from tiktok_uploader.upload import upload_videos
 from tiktok_uploader.auth import AuthBackend
 
+proxy_host = "45.41.171.86"
+proxy_port = "6122"
+proxy_username = "ffaspknt"
+proxy_password = "w9z2pl1j5c5h"
+proxy = {
+    'user': proxy_username,
+    'pass': proxy_password,
+    'host': proxy_host,
+    'port': proxy_port,
+}
+
 videos = [
     {
-        'video': 'video0.mp4',
+        'video': '../video/video1.mp4',
         'description': 'Video 1 is about ...'
-    },
-    {
-        'video': 'video1.mp4',
-        'description': 'Video 2 is about ...'
     }
 ]
 
-auth = AuthBackend(cookies='cookies.txt')
-failed_videos = upload_videos(videos=videos, auth=auth)
+auth = AuthBackend(cookies='../config/cookies.txt')
+failed_videos = upload_videos(videos=videos, auth=auth, proxy=proxy)
 
 for video in failed_videos:
     print(f'{video['video']} with description "{video['description']}" failed')
