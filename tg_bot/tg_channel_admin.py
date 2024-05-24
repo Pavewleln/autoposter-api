443902749724fa24a371af0389e68fad972d43a1
pavel <kulikovps2004@gmail.com>
2024-05-24 14:45:14 +0300

    fixed bugs: vers 1

--- tg_bot/tg_channel_admin.py
+++ tg_bot/tg_channel_admin.py
@@ -2,59 +2,49 @@ import asyncio
 import contextlib
 import logging
 import sqlite3
-
-from aiogram import Bot, Dispatcher, F, types
-from aiogram.types import Message, ChatJoinRequest
-from aiogram.filters import Command
-from aiogram.enums import ParseMode
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters.command import Command
+from aiogram.types import ChatJoinRequest
 
 API_TOKEN = '7159553101:AAHOpPtyFy7y-rDVHLSmc4e1G0LpYsRPa8Q'
 admin_id = 1042652647
 channel_id = -1002134206250
 
-"""
-    Принятие заявки и написание в лс пользователю
-"""
-async def approve_request(chat_join: ChatJoinRequest):
-    user_id = chat_join.from_user.id
-    msg = "Привет, ваш запрос на вступление одобрен!"
-    await bot.send_message(chat_id=user_id, text=msg)
-    await chat_join.approve()
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+dp.chat_join_request.filter(F.chat.id == channel_id)
+
+@dp.chat_join_request()
+async def process_join_request(request: ChatJoinRequest):
+    await request.approve()
+    await request.bot.send_message(request.from_user.id, "Мы приняли Вашу заявку!")
 
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
-    c.execute("INSERT INTO users VALUES (?)", [user_id])
+    c.execute("INSERT INTO users VALUES (?)", [request.from_user.id])
     conn.commit()
     conn.close()
 
-async def start():
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    bot = Bot(token=API_TOKEN)
-    dp = Dispatcher()
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
 
-    # Регистрация обработчика для принятия заявок на вступление в чат
-    dp.chat_join_request.register(approve_request, F.chat.id == channel_id)
+@dp.message(Command("send"))
+async def send_message_to_all_users(message: types.Message):
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute("SELECT user_id FROM users")
+    user_ids = c.fetchall()
+    conn.close()
+    text = message.text.replace("/send", "")
 
-    @dp.message(F.text, Command("send"))
-    async def send_message_to_all_users(message: types.Message):
-        conn = sqlite3.connect('users.db')
-        c = conn.cursor()
-        c.execute("SELECT user_id FROM users")
-        user_ids = c.fetchall()
-        conn.close()
-        text = message.text.replace("/send", "")
+    for user_id in user_ids:
+        await bot.send_message(chat_id=user_id[0], text=text)
 
-        for user_id in user_ids:
-            await bot.send_message(chat_id=user_id[0], text=text)
+async def main():
+    await dp.start_polling(bot)
 
-    try:
-        await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-    except Exception as ex:
-        logging.error(f'{ex}', exc_info=True)
-    finally:
-        await bot.session.close()
-
-if __name__ == '__main__':
-    with contextlib.suppress(KeyboardInterrupt, SystemExit):
-        asyncio.run(start())
+if __name__ == "__main__":
+    asyncio.run(main())
